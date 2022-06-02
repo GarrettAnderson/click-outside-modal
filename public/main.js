6c273e27f1be89d3a66de9da66c8815bc48ed7e1
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-06-02 18:20:21 -0400

    created variables for the inner and outer modal in order to access their data when opening the modal.

--- public/main.js
+++ public/main.js
@@ -1,10 +1,33 @@
-const cardButtons  = document.querySelector('.card button')
+const cardButtons  = document.querySelectorAll('.card button')
+const modalInner = document.querySelector('.modal-inner')
+const modalOuter = document.querySelector('.modal-outer')
 
 
-function handleCardButtonClick() {
+console.log(cardButtons)
+
+function handleCardButtonClick(event) {
   console.log('Ya clicked it')
+  const button = event.currentTarget
+  const card = button.closest('.card')
+  console.log(button)
+  console.log(card)
+
+  // grab the image source
+
+  const imgSrc = card.querySelector('img').src
+  console.log(imgSrc)
+
+  const desc = card.dataset.description
+  console.log(desc)
+
+  const name = card.querySelector('h2').textContent
+
+  // populate the modal with the new info
+  modalInner.innerHTML = `
+    <img src="${imgSrc.replace('200', '600')}" alt=""/>
+  `
 }
 
-cardButtons.forEach(button =>
-  button.addEventListener('click', handleCardButtonClick)
-)
+cardButtons.forEach(button => {
+    button.addEventListener('click', handleCardButtonClick)
+})
